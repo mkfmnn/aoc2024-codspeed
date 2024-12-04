d3a34b0123f26df376f8c8d6cd20846acb5b32a1
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-04 13:58:20 -0800

    bench: Rename functions for better view on CodSpeed dashboard

--- benches/advent2024.rs
+++ benches/advent2024.rs
@@ -6,24 +6,24 @@ use criterion::{criterion_group, criterion_main, Criterion};
 fn day1(c: &mut Criterion) {
     let input = include_str!("../data/input1.txt");
     let mut group = c.benchmark_group("day1");
-    group.bench_function("part1", |b| b.iter(|| day1::part1(black_box(&input))));
-    group.bench_function("part2", |b| b.iter(|| day1::part2(black_box(&input))));
+    group.bench_function("day1 part1", |b| b.iter(|| day1::part1(black_box(&input))));
+    group.bench_function("day1 part2", |b| b.iter(|| day1::part2(black_box(&input))));
     group.finish();
 }
 
 fn day2(c: &mut Criterion) {
     let input = include_str!("../data/input2.txt");
     let mut group = c.benchmark_group("day2");
-    group.bench_function("part1", |b| b.iter(|| day2::part1(black_box(&input))));
-    group.bench_function("part2", |b| b.iter(|| day2::part2(black_box(&input))));
+    group.bench_function("day2 part1", |b| b.iter(|| day2::part1(black_box(&input))));
+    group.bench_function("day2 part2", |b| b.iter(|| day2::part2(black_box(&input))));
     group.finish();
 }
 
 fn day3(c: &mut Criterion) {
     let input = include_str!("../data/input3.txt");
     let mut group = c.benchmark_group("day3");
-    group.bench_function("part1", |b| b.iter(|| day3::part1(black_box(&input))));
-    group.bench_function("part2", |b| b.iter(|| day3::part2(black_box(&input))));
+    group.bench_function("day3 part1", |b| b.iter(|| day3::part1(black_box(&input))));
+    group.bench_function("day3 part2", |b| b.iter(|| day3::part2(black_box(&input))));
     group.finish();
 }
 
