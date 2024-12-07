bc08d904d649ae616566e2bfe00b02b1d831b9f8
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-07 04:47:07 -0800

    day7: first implementation

--- src/day7.rs
+++ src/day7.rs
@@ -1,17 +1,49 @@
-pub fn part1(input: &str) -> usize {
-    part1_inner(input.as_bytes())
+pub fn part1(input: &str) -> u64 {
+    part_inner(input, recurse1)
 }
 
-fn part1_inner(bytes: &[u8]) -> usize {
-    0
+pub fn part2(input: &str) -> u64 {
+    part_inner(input, recurse2)
 }
 
-pub fn part2(input: &str) -> usize {
-    part2_inner(input.as_bytes())
+pub fn part_inner<F>(input: &str, f: F) -> u64
+where
+    F: Fn(u64, u64, &[u64]) -> bool,
+{
+    let mut sum = 0;
+    for line in input.lines() {
+        let (target, rest) = line.split_once(": ").unwrap();
+        let target = target.parse::<u64>().unwrap();
+        let nums = rest
+            .split(' ')
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect::<Vec<_>>();
+        let (&first, rest) = nums.split_first().unwrap();
+        if f(target, first, rest) {
+            sum += target;
+        }
+    }
+    sum
 }
 
-fn part2_inner(bytes: &[u8]) -> usize {
-    0
+fn recurse1(target: u64, accum: u64, rest: &[u64]) -> bool {
+    let Some((&first, rest)) = rest.split_first() else {
+        return target == accum;
+    };
+    recurse1(target, accum + first, rest) || recurse1(target, accum * first, rest)
+}
+
+fn recurse2(target: u64, accum: u64, rest: &[u64]) -> bool {
+    let Some((&first, rest)) = rest.split_first() else {
+        return target == accum;
+    };
+    recurse2(target, accum + first, rest)
+        || recurse2(target, accum * first, rest)
+        || recurse2(target, concat(accum, first), rest)
+}
+
+fn concat(a: u64, b: u64) -> u64 {
+    format!("{a}{b}").parse::<u64>().unwrap()
 }
 
 #[cfg(test)]
@@ -21,11 +53,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1(INPUT));
+        assert_eq!(4555081946288, part1(INPUT));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(INPUT));
+        assert_eq!(227921760109726, part2(INPUT));
     }
 }
