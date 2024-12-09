985daf578ed20c04e03e289ace0302a94802b722
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-09 01:01:56 -0800

    day8: restore working part 2 solution

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,8 @@
-use std::simd::{cmp::{SimdPartialEq, SimdPartialOrd}, i8x64, num::SimdUint};
+use std::simd::{
+    cmp::{SimdPartialEq, SimdPartialOrd},
+    i8x64,
+    num::SimdUint,
+};
 
 const DIM: usize = 50;
 const LINE_LEN: usize = DIM + 1;
@@ -29,9 +33,17 @@ pub fn part2(input: &str) -> usize {
 }
 
 #[inline(always)]
-fn set_station(stations: &mut [i8; 512], station_count: &mut [usize; 64], station: usize, bytes_offset: usize) {
+fn set_station(
+    stations: &mut [i8; 512],
+    station_count: &mut [usize; 64],
+    station: usize,
+    bytes_offset: usize,
+) {
     debug_assert!((1..=62).contains(&station));
-    let (x, y) = ((bytes_offset % LINE_LEN) as i8, (bytes_offset / LINE_LEN) as i8);
+    let (x, y) = (
+        (bytes_offset % LINE_LEN) as i8,
+        (bytes_offset / LINE_LEN) as i8,
+    );
     let c = station_count[station];
     stations[(c << 7) + station] = x;
     stations[(c << 7) + 64 + station] = y;
@@ -46,7 +58,7 @@ fn inner<const REPEAT: bool>(bytes: &[u8]) -> usize {
     let mut station_count = [0usize; 64];
     {
         let stations: &mut [i8; 512] = unsafe { std::mem::transmute(&mut stations_simd) };
-        let (prefix, batch, suffix) = bytes.as_simd::<{U8xRead::LEN}>();
+        let (prefix, batch, suffix) = bytes.as_simd::<{ U8xRead::LEN }>();
         for (i, &c) in prefix.iter().enumerate() {
             let s = LUT[c as usize] as usize;
             if s != 255 {
@@ -56,8 +68,12 @@ fn inner<const REPEAT: bool>(bytes: &[u8]) -> usize {
         let mut offset = prefix.len();
         for c in batch {
             // squash 0-9, A-Z, a-z into [1, 62]
-            let d = c.simd_gt(U8xRead::splat(b'9')).select(U8xRead::splat(b'A' - 1 - 10), U8xRead::splat(b'0' - 1));
-            let d = c.simd_gt(U8xRead::splat(b'Z')).select(U8xRead::splat(b'a' - 1 - 36), d);
+            let d = c
+                .simd_gt(U8xRead::splat(b'9'))
+                .select(U8xRead::splat(b'A' - 1 - 10), U8xRead::splat(b'0' - 1));
+            let d = c
+                .simd_gt(U8xRead::splat(b'Z'))
+                .select(U8xRead::splat(b'a' - 1 - 36), d);
             let c = c.saturating_sub(d);
             let mut m = c.simd_ne(U8xRead::splat(0)).to_bitmask();
             let a = c.to_array();
@@ -82,35 +98,79 @@ fn inner<const REPEAT: bool>(bytes: &[u8]) -> usize {
     }
 
     let mut antinodes = [0u64; DIM];
-    let min = i8x64::splat(0);
-    let max = i8x64::splat(DIM as i8);
-    for i in 0..4 {
-        for j in i+1..4 {
-            let ax = stations_simd[i << 1];
-            let ay = stations_simd[(i << 1) + 1];
-            let bx = stations_simd[j << 1];
-            let by = stations_simd[(j << 1) + 1];
-            let dx = bx - ax;
-            let dy = by - ay;
-            let n1x = bx + dx;
-            let n1y = by + dy;
-            let mut m = (n1x.simd_ge(min) & n1x.simd_lt(max) & n1y.simd_ge(min) & n1y.simd_lt(max)).to_bitmask();
-            let nodes = (n1x.to_array(), n1y.to_array());
-            while m != 0 {
-                let t = m.trailing_zeros() as usize;
-                antinodes[nodes.0[t] as usize] |= 1 << nodes.1[t];
+    if !REPEAT {
+        let min = i8x64::splat(0);
+        let max = i8x64::splat(DIM as i8);
+        for i in 0..4 {
+            for j in i + 1..4 {
+                let ax = stations_simd[i << 1];
+                let ay = stations_simd[(i << 1) + 1];
+                let bx = stations_simd[j << 1];
+                let by = stations_simd[(j << 1) + 1];
+                let dx = bx - ax;
+                let dy = by - ay;
+                let n1x = bx + dx;
+                let n1y = by + dy;
+                let mut m =
+                    (n1x.simd_ge(min) & n1x.simd_lt(max) & n1y.simd_ge(min) & n1y.simd_lt(max))
+                        .to_bitmask();
+                let nodes = (n1x.to_array(), n1y.to_array());
+                while m != 0 {
+                    let t = m.trailing_zeros() as usize;
+                    antinodes[nodes.0[t] as usize] |= 1 << nodes.1[t];
 
-                m &= !(1 << t);
+                    m &= !(1 << t);
+                }
+                let n2x = ax - dx;
+                let n2y = ay - dy;
+                let mut m =
+                    (n2x.simd_ge(min) & n2x.simd_lt(max) & n2y.simd_ge(min) & n2y.simd_lt(max))
+                        .to_bitmask();
+                let nodes = (n2x.to_array(), n2y.to_array());
+                while m != 0 {
+                    let t = m.trailing_zeros() as usize;
+                    antinodes[nodes.0[t] as usize] |= 1 << nodes.1[t];
+
+                    m &= !(1 << t);
+                }
             }
-            let n2x = ax - dx;
-            let n2y = ay - dy;
-            let mut m = (n2x.simd_ge(min) & n2x.simd_lt(max) & n2y.simd_ge(min) & n2y.simd_lt(max)).to_bitmask();
-            let nodes = (n2x.to_array(), n2y.to_array());
-            while m != 0 {
-                let t = m.trailing_zeros() as usize;
-                antinodes[nodes.0[t] as usize] |= 1 << nodes.1[t];
-
-                m &= !(1 << t);
+        }
+    } else {
+        let stations: &[i8; 512] = unsafe { std::mem::transmute(&stations_simd) };
+        for s in 1..=62 {
+            for i in 0..station_count[s] {
+                for j in 0..station_count[s] {
+                    if i == j {
+                        continue;
+                    }
+                    let a = (stations[(i << 7) + s], stations[(i << 7) + 64 + s]);
+                    let b = (stations[(j << 7) + s], stations[(j << 7) + 64 + s]);
+                    let delta = (b.0 - a.0, b.1 - a.1);
+                    let mut antinode = b;
+                    loop {
+                        antinodes[antinode.0 as usize] |= 1 << antinode.1;
+                        antinode = (antinode.0 + delta.0, antinode.1 + delta.1);
+                        if !(antinode.0 >= 0
+                            && antinode.0 < DIM as i8
+                            && antinode.1 >= 0
+                            && antinode.1 < DIM as i8)
+                        {
+                            break;
+                        }
+                    }
+                    antinode = a;
+                    loop {
+                        antinodes[antinode.0 as usize] |= 1 << antinode.1;
+                        antinode = (antinode.0 - delta.0, antinode.1 - delta.1);
+                        if !(antinode.0 >= 0
+                            && antinode.0 < DIM as i8
+                            && antinode.1 >= 0
+                            && antinode.1 < DIM as i8)
+                        {
+                            break;
+                        };
+                    }
+                }
             }
         }
     }
