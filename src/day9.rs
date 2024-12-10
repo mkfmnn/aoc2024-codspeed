d1455a58f53126bf9da92d3cc7ca6a0387cdf7df
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-09 23:46:03 -0800

    day9: more complex, optimized solutions

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 pub fn part1(input: &str) -> usize {
     part1_inner(input.as_bytes())
 }
@@ -49,65 +51,90 @@ pub fn part2(input: &str) -> usize {
     part2_inner(input.as_bytes())
 }
 
-struct Block {
-    id: usize,
-    start: usize,
-    len: usize,
-}
-
-struct FreeBlock {
-    start: usize,
-    len: usize,
-}
-
 fn part2_inner(mut bytes: &[u8]) -> usize {
     if bytes[bytes.len() - 1] == b'\n' {
         bytes = &bytes[..bytes.len() - 1];
     }
-    let mut blocks = Vec::<Block>::with_capacity(10001);
-    let mut free_blocks = Vec::<FreeBlock>::with_capacity(10000);
-    let mut i = 0;
+    let mut free_blocks: [BinaryHeap<Reverse<usize>>; 10] =
+        core::array::from_fn(|_| BinaryHeap::with_capacity(1000));
+    let mut free_idx = 0;
     let mut offset = 0;
-
-    let len = (bytes[i] - b'0') as usize;
-    blocks.push(Block {
-        id: i / 2,
-        start: offset,
-        len,
-    });
-    offset += len;
-    i += 1;
-    while i < bytes.len() {
-        let len = (bytes[i] - b'0') as usize;
-        free_blocks.push(FreeBlock { start: offset, len });
-        offset += len;
-        i += 1;
-        let len = (bytes[i] - b'0') as usize;
-        blocks.push(Block {
-            id: i / 2,
-            start: offset,
-            len,
-        });
-        offset += len;
-        i += 1;
-    }
-    for block in blocks.iter_mut().rev() {
-        for free in free_blocks.iter_mut() {
-            if free.start > block.start {
-                break;
-            }
-            if free.len >= block.len {
-                block.start = free.start;
-                free.start += block.len;
-                free.len -= block.len;
-                break;
-            }
-        }
+    let mut end_idx = bytes.len() - 1;
+    let mut end_offset = usize::MAX; // initially unknown; points to *end* of current index
+    if bytes[end_idx] == b'\n' {
+        end_idx -= 1;
     }
     let mut checksum = 0;
-    for block in blocks {
-        checksum += block.id * block.len * (2 * block.start + block.len - 1);
+    'outer: loop {
+        let id = end_idx / 2;
+        let len = (bytes[end_idx] - b'0') as usize;
+        if end_idx == free_idx {
+            end_offset = offset + len as usize;
+        }
+        // find the first free block that this block can fit in
+        // first check the free lists
+        let mut best = usize::MAX;
+        let mut best_len = 0;
+        for i in len..=9 {
+            if let Some(s) = free_blocks[i].peek() {
+                if s.0 < best {
+                    best_len = i;
+                    best = s.0;
+                }
+            }
+        }
+        if best != usize::MAX && best < end_offset {
+            let popped = free_blocks[best_len].pop().unwrap();
+            debug_assert_eq!(popped.0, best);
+            let start = best;
+            checksum += id * len * (2 * start + len - 1);
+            if best_len > len {
+                free_blocks[best_len - len].push(Reverse(best + len));
+            }
+            end_offset -= len;
+            end_offset -= (bytes[end_idx - 1] - b'0') as usize;
+            end_idx -= 2;
+            if end_idx == 0 {
+                break;
+            }
+            continue;
+        }
+        // if no block in free lists is big enough, scan for more
+        while end_idx <= free_idx {
+            offset += (bytes[free_idx] - b'0') as usize;
+            free_idx += 1;
+            let mut free_start = offset;
+            let mut free_len = (bytes[free_idx] - b'0') as usize;
+            offset += free_len;
+            free_idx += 1;
+            if end_idx == free_idx {
+                end_offset = offset + len as usize;
+            }
+
+            // is it big enough? if so use it immediately
+            if free_len >= len {
+                checksum += id * len * (2 * free_start + len - 1);
+                free_len -= len;
+                free_start += len;
+                if free_len > 0 {
+                    free_blocks[free_len].push(Reverse(free_start));
+                }
+                end_idx -= 2;
+                end_offset -= len + (bytes[end_idx + 1] - b'0') as usize;
+                continue 'outer;
+            } else if free_len > 0 {
+                free_blocks[free_len].push(Reverse(free_start));
+            }
+        }
+        // if we got here, no free block found, keep it in-place
+        checksum += id * len * (2 * end_offset - len - 1);
+        end_idx -= 2;
+        if end_idx == 0 {
+            break;
+        }
+        end_offset -= len + (bytes[end_idx + 1] - b'0') as usize;
     }
+    // don't have to deal with 0!
     checksum / 2
 }
 
@@ -124,6 +151,11 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(6337367222422, part1(INPUT));
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(2858, part2("2333133121414131402"));
     }
 
     #[test]
