5d2a0151889c6120fcd50b0217fa2632f3eeb681
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-10 12:21:52 -0800

    day10: initial impl

--- src/day10.rs
+++ src/day10.rs
@@ -3,7 +3,61 @@ pub fn part1(input: &str) -> usize {
 }
 
 fn part1_inner(bytes: &[u8]) -> usize {
-    0
+    let dim = (bytes.len() as f64).sqrt() as usize;
+    assert!(dim < 64);
+    let line = dim + 1;
+    let mut sum = 0;
+    for i in 0..bytes.len() {
+        if bytes[i] == b'0' {
+            let mut visited = [0u64; 64];
+            sum += part1_recurse(bytes, &mut visited, line, i, b'0');
+        }
+    }
+    sum
+}
+
+fn part1_recurse(
+    bytes: &[u8],
+    visited: &mut [u64; 64],
+    line: usize,
+    pos: usize,
+    char: u8,
+) -> usize {
+    let (v1, v2) = (pos / 64, pos % 64);
+    if visited[v1] & (1 << v2) != 0 {
+        return 0;
+    }
+    visited[v1] |= 1 << v2;
+    if char == b'9' {
+        return 1;
+    }
+    let nextc = char + 1;
+    let mut sum = 0;
+    if pos >= line {
+        let next = pos - line;
+        if bytes[next] == nextc {
+            sum += part1_recurse(bytes, visited, line, next, nextc);
+        }
+    }
+    if pos >= 1 {
+        let next = pos - 1;
+        if bytes[next] == nextc {
+            sum += part1_recurse(bytes, visited, line, next, nextc);
+        }
+    }
+    if pos + 1 < bytes.len() {
+        let next = pos + 1;
+        if bytes[next] == nextc {
+            sum += part1_recurse(bytes, visited, line, next, nextc);
+        }
+    }
+    if pos + line < bytes.len() {
+        let next = pos + line;
+        if bytes[next] == nextc {
+            sum += part1_recurse(bytes, visited, line, next, nextc);
+        }
+    }
+    sum
 }
 
 pub fn part2(input: &str) -> usize {
@@ -11,7 +65,49 @@ pub fn part2(input: &str) -> usize {
 }
 
 fn part2_inner(bytes: &[u8]) -> usize {
-    0
+    let dim = (bytes.len() as f64).sqrt() as usize;
+    assert!(dim < 64);
+    let line = dim + 1;
+    let mut sum = 0;
+    for i in 0..bytes.len() {
+        if bytes[i] == b'0' {
+            sum += part2_recurse(bytes, line, i, b'0');
+        }
+    }
+    sum
+}
+
+fn part2_recurse(bytes: &[u8], line: usize, pos: usize, char: u8) -> usize {
+    if char == b'9' {
+        return 1;
+    }
+    let nextc = char + 1;
+    let mut sum = 0;
+    if pos >= line {
+        let next = pos - line;
+        if bytes[next] == nextc {
+            sum += part2_recurse(bytes, line, next, nextc);
+        }
+    }
+    if pos >= 1 {
+        let next = pos - 1;
+        if bytes[next] == nextc {
+            sum += part2_recurse(bytes, line, next, nextc);
+        }
+    }
+    if pos + 1 < bytes.len() {
+        let next = pos + 1;
+        if bytes[next] == nextc {
+            sum += part2_recurse(bytes, line, next, nextc);
+        }
+    }
+    if pos + line < bytes.len() {
+        let next = pos + line;
+        if bytes[next] == nextc {
+            sum += part2_recurse(bytes, line, next, nextc);
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
