9cfff74a6ff7a0da8bd069ff5cf543a137da474b
Miles Kaufmann <mkfmnn@gmail.com>
2024-12-11 13:20:20 -0800

    day11: initial solution

--- src/day11.rs
+++ src/day11.rs
@@ -1,17 +1,63 @@
-pub fn part1(input: &str) -> usize {
-    part1_inner(input.as_bytes())
+use std::collections::HashMap;
+
+pub fn parse(input: &str) -> impl Iterator<Item = usize> + use<'_> {
+    input
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
 }
 
-fn part1_inner(bytes: &[u8]) -> usize {
-    0
+pub fn part1(input: &str) -> usize {
+    let mut stones = parse(input).collect::<Vec<_>>();
+    for _ in 0..25 {
+        stones = blink(&stones);
+    }
+    stones.len()
+}
+
+fn blink(input: &[usize]) -> Vec<usize> {
+    let mut out = Vec::new();
+    for &n in input {
+        if n == 0 {
+            out.push(1);
+        } else {
+            let digits = n.ilog10() + 1;
+            if digits % 2 == 0 {
+                let mask = 10usize.pow(digits / 2);
+                out.push(n / mask);
+                out.push(n % mask);
+            } else {
+                out.push(n * 2024);
+            }
+        }
+    }
+    out
 }
 
 pub fn part2(input: &str) -> usize {
-    part2_inner(input.as_bytes())
+    let mut cache = HashMap::<(usize, usize), usize>::new();
+    parse(input).map(|n| expand(n, 75, &mut cache)).sum()
 }
 
-fn part2_inner(bytes: &[u8]) -> usize {
-    0
+fn expand(n: usize, it: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    if it == 0 {
+        return 1;
+    }
+    if let Some(r) = cache.get(&(n, it)) {
+        return *r;
+    }
+    let r = if n == 0 {
+        expand(1, it - 1, cache)
+    } else {
+        let digits = n.ilog10() + 1;
+        if digits % 2 == 0 {
+            let mask = 10usize.pow(digits / 2);
+            expand(n / mask, it - 1, cache) + expand(n % mask, it - 1, cache)
+        } else {
+            expand(n * 2024, it - 1, cache)
+        }
+    };
+    cache.insert((n, it), r);
+    return r;
 }
 
 #[cfg(test)]
@@ -21,11 +67,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1(INPUT));
+        assert_eq!(217443, part1(INPUT));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(INPUT));
+        assert_eq!(257246536026785, part2(INPUT));
     }
 }
